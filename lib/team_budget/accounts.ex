cb532d516d12403a1b368c2a0f5f3f937508097d
Guilherme Rigotti <ggrigotti@gmail.com>
2021-07-19 14:08:34 -0300

    finish create_user mutation

--- lib/team_budget/accounts.ex
+++ lib/team_budget/accounts.ex
@@ -5,4 +5,10 @@ defmodule TeamBudget.Accounts do
   def list_users() do
     Repo.all(User)
   end
+
+  def create_user(user) do
+    user
+    |> User.changeset()
+    |> Repo.insert!()
+  end
 end
