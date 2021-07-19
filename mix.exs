50e06e2c23a82467fbf67105d7f34b752d8d769e
Guilherme Rigotti <ggrigotti@gmail.com>
2021-07-19 11:40:46 -0300

    fix elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TeamBudget.MixProject do
     [
       app: :team_budget,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
